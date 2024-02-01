39771c93a54525ccca237403dd417e062acc99fb
johnlivingprooff <johnlivingprooff@gmail.com>
2024-02-01 22:32:20 +0200

    do_deploy - fix

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,45 +1,50 @@
 #!/usr/bin/python3
 """combines do_pack() with do_deply()"""
 from fabric.api import *
-from datetime import datetime
 import os
-
-
 env.hosts = ['100.25.162.17', '100.25.131.228']
 env.user = "ubuntu"
-
-
-def do_pack():
-    """returns .tgz archive"""
-
-    local("mkdir -p versions")
-    date = datetime.now().strftime("%Y%m%d%H%M%S")
-    archive = f"versions/web_static_{date}.tgz"
-
-    zip_arch = local(f"tar -cvzf {archive} web_static")
-    if zip_arch.succeeded:
-        return archive
-    else:
-        return None
 
 
 def do_deploy(archive_path):
     """Distributes an archive to your web servers"""
 
-    if os.path.exists(archive_path):
-        put(archive_path, '/tmp/')
-        archive_name = archive_path.split('/')[-1]
-        dirN = archive_name.split('.')[0]
-        release_path = '/data/web_static/releases/'
+    if not os.path.exists(archive_path):
+        return False
 
-        run(f'mkdir -p {release_path}{dirN}')
-        run(f'tar -xzf /tmp/{archive_name} -C {release_path}{dirN}/')
-        run(f'rm /tmp/{archive_name}')
-        run(f'mv {release_path}{dirN}/web_static/* {release_path}{dirN}/')
-        run('rm -rf /data/web_static/current')
-        run(f'ln -s {release_path}{dirN}/ /data/web_static/current')
+    try:
+        b = put(archive_path, '/tmp/')
+        if b.failed:
+            return False
+        archive_name = os.path.basename(archive_path)
+        dirN = os.path.splitext(archive_name)[0]
+        release_path = f'/data/web_static/releases/{dirN}'
+
+        b = run(f'mkdir -p {release_path}{dirN}')
+        if b.failed:
+            return False
+        b = run(f'tar -xzf /tmp/{archive_name} -C {release_path}/')
+        if b.failed:
+            return False
+        b = run(f'rm /tmp/{archive_name}')
+        if b.failed:
+            return False
+        b = run(f'mv {release_path}/web_static/* {release_path}/')
+        if b.failed:
+            return False
+        b = run(f'rm -rf {release_path}/web_static')
+        if b.failed:
+            return False
+        b = run('rm -rf /data/web_static/current')
+        if b.failed:
+            return False
+        b = run(f'ln -s {release_path}/ /data/web_static/current')
+        if b.failed:
+            return False
 
         print('New version deployed!')
         return True
 
-    return False
+    except Exception as e:
+        print(e)
+        return False

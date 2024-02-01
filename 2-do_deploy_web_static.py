5a99c37b3ff434e20abfad375c3140c4bcdf367f
johnlivingprooff <johnlivingprooff@gmail.com>
2024-02-01 23:42:37 +0200

    last commit :(

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""combines do_pack() with do_deply()"""
+"""Distribution module"""
 from fabric.api import *
 import os
 env.hosts = ['100.25.162.17', '100.25.131.228']
@@ -12,34 +12,33 @@ def do_deploy(archive_path):
     if not os.path.exists(archive_path):
         return False
 
-
-    b = put(archive_path, '/tmp/')
-    if b.failed:
+    err = put(archive_path, '/tmp/')
+    if err.failed:
         return False
     archive_name = os.path.basename(archive_path)
     dirN = os.path.splitext(archive_name)[0]
     release_path = f'/data/web_static/releases/{dirN}'
 
-    b = run(f'mkdir -p {release_path}{dirN}')
-    if b.failed:
+    err = run(f'mkdir -p {release_path}{dirN}')
+    if err.failed:
         return False
-    b = run(f'tar -xzf /tmp/{archive_name} -C {release_path}/')
-    if b.failed:
+    err = run(f'tar -xzf /tmp/{archive_name} -C {release_path}/')
+    if err.failed:
         return False
-    b = run(f'rm /tmp/{archive_name}')
-    if b.failed:
+    err = run(f'rm /tmp/{archive_name}')
+    if err.failed:
         return False
-    b = run(f'mv {release_path}/web_static/* {release_path}/')
-    if b.failed:
+    err = run(f'mv {release_path}/web_static/* {release_path}/')
+    if err.failed:
         return False
-    b = run(f'rm -rf {release_path}/web_static')
-    if b.failed:
+    err = run(f'rm -rf {release_path}/web_static')
+    if err.failed:
         return False
-    b = run('rm -rf /data/web_static/current')
-    if b.failed:
+    err = run('rm -rf /data/web_static/current')
+    if err.failed:
         return False
-    b = run(f'ln -s {release_path}/ /data/web_static/current')
-    if b.failed:
+    err = run(f'ln -s {release_path}/ /data/web_static/current')
+    if err.failed:
         return False
 
     print('New version deployed!')

c0f5f936b0ed22bfecd11727cc2e29a91d1b9595
johnlivingprooff <johnlivingprooff@gmail.com>
2024-02-18 20:11:37 +0200

    9. Cities by states

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -14,13 +14,13 @@ def teardown(exception):
     storage.close()
 
 
-@app.route('/states_list', strict_slashes=False)
-def states_list():
-    """ displays a list of the states """
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_by_states():
+    """ displays a list of the cities by states """
     states = storage.all(State).values()
     sorted_states = sorted(states, key=lambda x: x.name)
 
-    return render_template('7-states_list.html', states=sorted_states)
+    return render_template('8-cities_by_states.html', states=sorted_states)
 
 
 if __name__ == '__main__':
